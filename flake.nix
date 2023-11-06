c071336249f996a2d5fd0c53a2e982e5ed30d7b1
Evan Goode <mail@evangoo.de>
2023-11-06 15:32:58 -0500

    Nix Flake, poetry

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Duolibre";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          duolibre = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.duolibre;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.poetry ];
+        };
+      });
+}
